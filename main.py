1397fbf9add7ff4884be6916c5b8622ef5ea32ac
Nick Riley <nicholas@rileysfarm.com>
2019-10-28 22:53:02 -0700

    Occurrences implementation. (#3)

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ def load():
             DISCOGRAPHY.append(item)
 
 def occurrences(word):
-    print("'ye heh' is not implemented yet.")
+    full_text = " ".join(
+        [d for d in [l for a, s, l in DISCOGRAPHY]]
+    ).lower()
+    count = full_text.count(word.lower())
+    return count
 
 def occurrences_verbose(word):
     print("'ye HEH!' is not implemented yet.")
 
 def main():
+    load()
     if len(sys.argv) != 3 :
         print("Wrong number of arguments specified")
         sys.exit(1)
 
     if "heh" in sys.argv[1]:
-        occurrences(sys.argv[2])
+        print(occurrences(sys.argv[2]))
     elif "HEH!" in sys.argv[1]:
         occurrences_verbose(sys.argv[2])
     else:
         print("Unrecognized command: '" + sys.argv[3] + "'")
         sys.exit(1)
-
-
-    load()
 
 if __name__ == "__main__":
     main()
