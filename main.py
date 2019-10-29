aac0f635eddf6438a51095c08e3615f5aa614fbf
Curtis Hohl <curtisphohl@gmail.com>
2019-10-29 16:14:54 -0700

    added individual song counts for occurances_verbose

--- main.py
+++ main.py
@@ -17,18 +17,28 @@ def occurrences(word):
     return count
 
 def occurrences_verbose(word):
-    print("'ye HEH!' is not implemented yet.")
+    result = ""
+    song_counts = []
+    for a, s, l in DISCOGRAPHY:
+        song_occurance = l.count(word.lower())
+        if song_occurance > 0:
+            song_counts.append([a, s, song_occurance])
+    for a, s, song_occurance in song_counts:
+        result += a + ", " + s + ": " + str(song_occurance) + "\n"
+    return result 
 
 def main():
+    if "main.py" in sys.argv: sys.argv.remove("main.py") #for easy debugging
     load()
     if len(sys.argv) != 3 :
+        print(sys.argv)
         print("Wrong number of arguments specified")
         sys.exit(1)
 
     if "heh" in sys.argv[1]:
         print(occurrences(sys.argv[2]))
     elif "HEH!" in sys.argv[1]:
-        occurrences_verbose(sys.argv[2])
+        print(occurrences_verbose(sys.argv[2]))
     else:
         print("Unrecognized command: '" + sys.argv[3] + "'")
         sys.exit(1)
