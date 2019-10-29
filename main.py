67a054eedab453db080126e96bedc5ca3d69cca7
Nick Riley <nicholas@rileysfarm.com>
2019-10-28 22:06:02 -0700

    Command API (#2)
    * Initial discography load implementation.
    
    * Added unimplimented command API.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 
 DISCOGRAPHY = []
 
@@ -8,8 +9,28 @@ def load():
             item = (raw[0], raw[1], raw[2])
             DISCOGRAPHY.append(item)
 
+def occurrences(word):
+    print("'ye heh' is not implemented yet.")
+
+def occurrences_verbose(word):
+    print("'ye HEH!' is not implemented yet.")
+
 def main():
+    if len(sys.argv) != 3 :
+        print("Wrong number of arguments specified")
+        sys.exit(1)
+
+    if "heh" in sys.argv[1]:
+        occurrences(sys.argv[2])
+    elif "HEH!" in sys.argv[1]:
+        occurrences_verbose(sys.argv[2])
+    else:
+        print("Unrecognized command: '" + sys.argv[3] + "'")
+        sys.exit(1)
+
+
     load()
 
-main()
+if __name__ == "__main__":
+    main()
     
