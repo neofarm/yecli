265899ce08fb189e864485914487394460a84cf1
Curtis Hohl <curtisphohl@gmail.com>
2019-10-30 07:12:41 -0700

    Added album total counts to verbose (#7)
    * added individual song counts for occurances_verbose
    
    * added album total counts to verbose

--- ye.py
+++ ye.py
@@ -19,18 +19,27 @@ def occurrences(word):
     return count
 
 def occurrences_verbose(word):
-    result = ""
-    song_counts = []
+    song_counts = [] #[album_name, song_name, count]
+    album_counts = {} #{album_name: count}
     for a, s, l in DISCOGRAPHY:
         song_occurance = l.count(word.lower())
         if song_occurance > 0:
             song_counts.append([a, s, song_occurance])
-    for a, s, song_occurance in song_counts:
-        result += a + ", " + s + ": " + str(song_occurance) + "\n"
-    return result 
+            if a in album_counts:
+                album_counts[a] += song_occurance
+            else: album_counts[a] = song_occurance
+    return resultify_verbose_counts(song_counts, album_counts)
+
+def resultify_verbose_counts(song_counts, album_counts):
+    result = ""
+    for a, c in album_counts.items():
+        result += a + ": " + str(c) + "\n"
+        for al, s, o in (y for y in song_counts if y[0] == a):
+            result += "   " + s + ": " + str(o) + "\n"
+    return result
 
 def main():
-    if "main.py" in sys.argv: sys.argv.remove("main.py") #for easy debugging
+    if "ye.py" in sys.argv: sys.argv.remove("ye.py") #for easy debugging
     load()
     if len(sys.argv) != 3 :
         print(sys.argv)
